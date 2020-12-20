0b2e30aaf81ee037c09d7f39030c660ab3fdc4a1
Johann Bernhardt <johann.bernhardt@tum.de>
2020-12-20 01:14:44 +0100

    refactored driver into source folder such that the project is easier set up

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,13 @@ void wifi_monitoring_cb(tstrM2MWifiRxPacketInfo *pstrWifiRxPacket, uint8 *pu8Pay
     if((NULL != pstrWifiRxPacket) && (0 != u16PayloadSize)) {
         if(MANAGEMENT == pstrWifiRxPacket->u8FrameType) {
             M2M_INFO("***# MGMT PACKET #***\n");
+            Serial.println("Management Packet");
         } else if(DATA_BASICTYPE == pstrWifiRxPacket->u8FrameType) {
             M2M_INFO("***# DATA PACKET #***\n");
+            Serial.println("Data Packet");
         } else if(CONTROL == pstrWifiRxPacket->u8FrameType) {
             M2M_INFO("***# CONTROL PACKET #***\n");
+            Serial.println("Control Packet");
         }
     }
 }
@@ -35,13 +38,14 @@ int main()
     param.pfAppWifiCb = wifi_cb;
     param.pfAppMonCb  = wifi_monitoring_cb;
     
+    // init Board Support Package
     nm_bsp_init();
     
     if(!m2m_wifi_init(&param)) {
         //Enable Monitor Mode with filter to receive all data frames on channel 1
         tstrM2MWifiMonitorModeCtrl	strMonitorCtrl = {0};
-        strMonitorCtrl.u8ChannelID		= M2M_WIFI_CH_1;
-        strMonitorCtrl.u8FrameType		= DATA_BASICTYPE;
+        strMonitorCtrl.u8ChannelID		= M2M_WIFI_CH_ALL;
+        strMonitorCtrl.u8FrameType		= DATA_BASICTYPE;  // M2M_WIFI_FRAME_TYPE_ANY
         strMonitorCtrl.u8FrameSubtype	= M2M_WIFI_FRAME_SUB_TYPE_ANY; //Receive any subtype of data frame
         m2m_wifi_enable_monitoring_mode(&strMonitorCtrl, gmgmt, sizeof(gmgmt), 0);
         
