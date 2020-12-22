b5be63104801e0f0816b1688b8873ef868750832
Johann Bernhardt <johann.bernhardt@tum.de>
2020-12-22 14:52:48 +0100

    Bugfix. Now sends and receives raw wifi, but windoof cannot match it. Gotta go back to udp :-(

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
-#define CONF_WINC_DEBUG 1
-#define SERIAL_DEBUG
+#define SERIAL_DEBUG 1
+#define CONF_MGMT 1
 
 extern "C"{
     #include "driver/include/m2m_wifi.h"
@@ -16,6 +16,15 @@ inline void LOG(const char* s)
 #endif
 }
 
+struct ethheader {
+    unsigned char       dsta[6];
+    unsigned char       srca[6];
+    uint16_t            type;
+};
+
+static const uint8_t IMAGINARY_MAC_ADDR[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0x75, 0x67};
+static const uint8_t LAPTOP_MAC_ADDR[6] = {0x84, 0x3A, 0x4B, 0x7B, 0xD2, 0x30};
+
 //Declare receive buffer 
 uint8 gmgmt[1600];
 
@@ -28,11 +37,11 @@ void wifi_monitoring_cb(tstrM2MWifiRxPacketInfo *pstrWifiRxPacket, uint8 *pu8Pay
 {
     if((NULL != pstrWifiRxPacket) && (0 != u16PayloadSize)) {
         if(MANAGEMENT == pstrWifiRxPacket->u8FrameType) {
-            LOG("Management Packet");
+            M2M_INFO("***# MGMT PACKET #***\n");
         } else if(DATA_BASICTYPE == pstrWifiRxPacket->u8FrameType) {
-            LOG("Data Packet");
+            M2M_INFO("***# DATA PACKET #***\n");
         } else if(CONTROL == pstrWifiRxPacket->u8FrameType) {
-            LOG("Control Packet");
+            M2M_INFO("***# CONTROL PACKET #***\n");
         }
     }
 }
@@ -58,12 +67,13 @@ void setup()
     LOG("WIFI init");
 
     sint8 ret = m2m_wifi_init(&param);
-    if(ret != M2M_SUCCESS) {
+    if(ret == M2M_SUCCESS) {
         //Enable Monitor Mode with filter to receive all data frames on channel 1
         tstrM2MWifiMonitorModeCtrl	strMonitorCtrl = {0};
-        strMonitorCtrl.u8ChannelID		= M2M_WIFI_CH_1;//M2M_WIFI_CH_ALL;
-        strMonitorCtrl.u8FrameType		= DATA_BASICTYPE;  // M2M_WIFI_FRAME_TYPE_ANY
-        strMonitorCtrl.u8FrameSubtype	= M2M_WIFI_FRAME_SUB_TYPE_ANY; //Receive any subtype of data frame
+        strMonitorCtrl.u8ChannelID		= M2M_WIFI_CH_10;//M2M_WIFI_CH_ALL;    12, 11
+        strMonitorCtrl.u8FrameType		= M2M_WIFI_FRAME_TYPE_ANY;//DATA_BASICTYPE;
+        strMonitorCtrl.u8FrameSubtype	= M2M_WIFI_FRAME_SUB_TYPE_ANY;
+        //memcpy(strMonitorCtrl.au8DstMacAddress, LAPTOP_MAC_ADDR, sizeof(LAPTOP_MAC_ADDR));
         LOG("switch to monitoring mode");
         if(m2m_wifi_enable_monitoring_mode(&strMonitorCtrl, gmgmt, sizeof(gmgmt), 0) != M2M_SUCCESS)
         {
@@ -81,19 +91,25 @@ void setup()
 
 void loop()
 {
-    /*
     uint8 txBuffer[100];
     uint16 headerLength = 14;
-    for(uint16 i = 0;i<14;i++)
-        txBuffer[i] = 0xFF;
-    LOG("Send marker packet");
-    m2m_wifi_send_wlan_pkt( txBuffer, headerLength, 100);
-    */
+    ethheader* eth = (ethheader*)txBuffer;
+    memcpy(eth->dsta, LAPTOP_MAC_ADDR, sizeof(LAPTOP_MAC_ADDR));
+    memcpy(eth->srca, IMAGINARY_MAC_ADDR, sizeof(IMAGINARY_MAC_ADDR));
+    eth->type = 0x8858;
+    
     
     while(1) {
         if(m2m_wifi_handle_events(NULL) != M2M_SUCCESS)
         {
             LOG("Error handling events!");
+        }
+        static int i = 0;
+        i = (i+1) % 1000000;
+        if(i == 0)
+        {
+            LOG("Send marker packet");
+            m2m_wifi_send_wlan_pkt( txBuffer, headerLength, 100);
         }
     }  
 }
